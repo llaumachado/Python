3a82730a7a13a6a28099a30a63594d31a866aef2
Laura Machado <llaura.machado08@gmail.com>
2024-09-10 10:51:44 -0300

    Add files via upload

---
+++ 05-aula.py
@@ -0,0 +1,13 @@
+# Tipos de dados em python
+# Inteiro
+inteiro = 10
+print (inteiro)
+# floats
+float = 10.5
+print(float)
+# String
+string = "Olá, mundo"
+print(string)
+# Booleano
+booleano = True
+print(booleano)
