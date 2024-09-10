d81ec7ab09b144068f2793ff94181a63f740ee8c
Laura Machado <llaura.machado08@gmail.com>
2024-09-10 11:15:25 -0300

    Add files via upload

---
+++ 06-aula.py
@@ -0,0 +1,15 @@
+#operação aritmeticos
+#soma
+print(10 + 5)
+#subtração
+print(10 - 5)
+#multiplicação
+print(10 * 5)
+#divisão
+print(10 / 5)
+# divisão inteira
+print(10 // 5)
+#exponenciação
+print(10 ** 5)
+#módulo\ resto da divisão
+print(10 % 5)
