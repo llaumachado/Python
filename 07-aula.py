0af80a63985160a8f6b796d9bb3ce0d97a63a61b
Laura Machado <llaura.machado08@gmail.com>
2024-09-11 11:12:00 -0300

    Add files via upload

---
+++ 07-aula.py
@@ -0,0 +1,14 @@
+nota1 = float(input("digite a nota 1:"))
+nota2 = float(input("digite a nota 2:"))
+nota3 = float(input("digite a nota 3:"))
+nota4 = float(input("digite a nota 4:"))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f"a media final obtidas = {media}")
+
+if(media >= 80):
+    print("aprovado")
+else:
+    print("Reprovado")
+  
+
