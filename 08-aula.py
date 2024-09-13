f62cb9434051e66d1df73a1e6b8214fe57cb716e
Laura Machado <llaura.machado08@gmail.com>
2024-09-13 09:26:33 -0300

    Add files via upload

---
+++ 08-aula.py
@@ -0,0 +1,13 @@
+# atribuição de variavel 
+a = 5
+b = 10
+
+print(f"valor de A ={a} e o valor de B= {b}")
+print("Trocando valores...")
+auxiliar = a 
+a = b 
+b = auxiliar
+print(f"Valor de A ={a} e o Valor de B= {b}")
+
+#
+
