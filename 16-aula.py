39f2f87ff525f45d48d8ecb73960c9366c5e8f19
Laura Machado <llaura.machado08@gmail.com>
2024-09-26 11:42:00 -0300

    Add files via upload

---
+++ 16-aula.py
@@ -0,0 +1,9 @@
+def somar(a,b):
+    return a + b
+
+print("algoritmo de soma")
+valor1 = int(input("digite o valor1:"))
+valor2 = int(input("digite o valor2:"))
+resultado = somar (valor1,valor2)
+
+print(f"o resultado da soma = {resultado}")
